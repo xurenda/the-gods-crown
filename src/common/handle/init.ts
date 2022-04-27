8cdf25bd9f89356eee35eaadc1f6db78d93c5ce7
xurenda <renda.xu@xiaopiu.com>
2022-04-27 11:55:43 +0800

    log

--- src/common/handle/init.ts
+++ src/common/handle/init.ts
@@ -4,20 +4,24 @@ export default function initGlobal() {
   // process.env.NODE_ENV: 'development' | 'production' | 'test'
   window.isDev = process.env.NODE_ENV === 'development'
 
-  window.log = log
-  window.logTrace = (...args: any[]) => {
-    if (!window.isDev) return
-
-    log(args)
-    console.trace()
-  }
+  window.log = (...args: any[]) => log(false, ...args)
+  window.logTrace = (...args: any[]) => log(true, ...args)
 }
 
-function log(...args: any[]) {
+function log(trace: boolean, ...args: any[]) {
   if (!window.isDev) return
 
-  const key = args.shift()
+  let key = 'DEBUG'
+  const firstArg = args[0]
+  if (firstArg != null && typeof firstArg !== 'object') {
+    key = firstArg.toString()
+    args.shift()
+  }
 
-  console.log(`%c[${key.toString()}] ${dateFormat(new Date(), 'HH:mm:ss.SSS')}`, `color: white; background: black; padding: 4px 10px; font-size: 12px;`)
+  console.log(
+    `%c[${key}${trace ? ' - trace' : ''}] ${dateFormat(new Date(), 'HH:mm:ss.SSS')}`,
+    `color: white; background: black; padding: 4px 10px; font-size: 12px;`
+  )
   console.log(...args)
+  trace && console.trace()
 }
